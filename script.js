89d42f68e12384eea40f7e6d6cc783d1773aa964
ahmedgalal8540 <ahmed.galal8540@gmail.com>
2023-06-16 19:45:57 +0200

    new commit and edit

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const uploadForm = document.getElementById('uploadForm');
-    const fileInput = document.getElementById('fileInput');
-    const fileList = document.getElementById('fileList');
+    const postForm = document.getElementById('postForm');
+    const titleInput = document.getElementById('titleInput');
+    const contentInput = document.getElementById('contentInput');
+    const imageInput = document.getElementById('imageInput');
+    const postList = document.getElementById('postList');
 
-    uploadForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const files = fileInput.files;
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const listItem = document.createElement('li');
-        listItem.textContent = file.name;
-        fileList.appendChild(listItem);
-    }
-    uploadForm.reset();
+    postForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const title = titleInput.value;
+        const content = contentInput.value;
+        const image = imageInput.files[0];
+        const post = createPost(title, content, image);
+        postList.prepend(post);
+        postForm.reset();
     });
+
+    function createPost(title, content, image) {
+        const post = document.createElement('li');
+        post.classList.add('post');
+
+        const heading = document.createElement('h3');
+        heading.textContent = title;
+        post.appendChild(heading);
+
+        if (image) {
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(image);
+            img.alt = title;
+            post.appendChild(img);
+        }
+
+        const paragraph = document.createElement('p');
+        paragraph.textContent = content;
+        post.appendChild(paragraph);
+
+        return post;
+    }
 });
